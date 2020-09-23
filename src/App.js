0105511a4d0f18cc897aa5d354aae2cdce1720c1
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-09-23 15:06:30 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SpinningWheel from "./components/SpinningWheel";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SpinningWheel />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SpinningWheel.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from "./SpinningWheel.styled";
+
+export default function SpinningWheel() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
